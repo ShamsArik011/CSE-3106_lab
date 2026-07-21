765d2067f3678f44c85eebc9b88e80ed1d7b516b
ShamsArik <arikshahariar@gmail.com>
2026-07-21 14:11:54 +0600

    add function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
-#include<iostream>
+
+#include <iostream>
 using namespace std;
+
+int add(int a,int b)
+{
+    return a+b;
+}
 
 int main()
 {
-    cout<<"Hello Git";
+    cout<<"Calculator"<<endl;
+
+    cout<<add(5,4);
 }
